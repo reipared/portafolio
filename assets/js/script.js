c9f02e45e792fa02c4a5ecace71ff5af2dde0bfa
rei.pared <reinaldopared@gmail.com>
2022-12-06 16:23:03 -0400

    Update website

---
+++ assets/js/script.js
@@ -0,0 +1,58 @@
+"use strict";
+
+// Add event listener on multiple elements
+
+const addEventOnElements = function (elements, eventType, callback) {
+	for (let i = 0, len = elements.length; i < len; i++) {
+		elements[i].addEventListener(eventType, callback);
+	}
+};
+
+// Navbar toggle for mobile
+
+const navbar = document.querySelector("[data-navbar]");
+const navTogglers = document.querySelectorAll("[data-nav-toogler]");
+const overlay = document.querySelector("[data-overlay]");
+
+const toggleNavbar = function () {
+	navbar.classList.toggle("active");
+	overlay.classList.toggle("active");
+	document.body.classList.toggle("nav-active");
+};
+
+addEventOnElements(navTogglers, "click", toggleNavbar);
+
+// HEADER
+// active header when window scroll down to 100px
+
+const header = document.querySelector("[data-header]");
+
+window.addEventListener("scroll", function () {
+	if (window.scrollY > 100) {
+		header.classList.remove("active");
+	}
+});
+
+// Scroll Reveal
+
+const revealElements = document.querySelectorAll("[data-reveal]");
+const revealDelayElements = document.querySelectorAll("[data-reveal-delay]");
+
+const reveal = function () {
+	for (let i = 0, len = revealElements.length; i < len; i++) {
+		if (
+			revealElements[i].getBoundingClientRect().top <
+			window.innerHeight / 1.2
+		) {
+			revealElements[i].classList.add("revealed");
+		}
+	}
+};
+
+for (let i = 0, len = revealDelayElements.length; i < len; i++) {
+	revealDelayElements[i].style.transitionDelay =
+		revealDelayElements[i].dataset.revealDelay;
+}
+
+window.addEventListener("scroll", reveal);
+window.addEventListener("load", reveal);
